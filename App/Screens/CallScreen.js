106c395692a6fdd0f95ead6e09ff8be1a5efb35a
PUNNYA PRADEEP <punnyapradeep1328@gmail.com>
2024-08-13 10:30:00 +0530

    ...

--- App/Screens/CallScreen.js
+++ App/Screens/CallScreen.js
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
     marginTop: 30,
+    paddingBottom:20
   },
   filterContainer: {
     flexDirection: 'row',
